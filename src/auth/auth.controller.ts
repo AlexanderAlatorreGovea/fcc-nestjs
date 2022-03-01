043624034ed0c1a1cf7a4a7a768be0d542af6082
Alexander <Alexander_alatorre1993@hotmail.com>
2022-02-28 21:20:30 -0700

    added argon2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,11 +8,11 @@ export class AuthController {
 
   @Post('signup')
   signup(@Body() dto: AuthDto) {
-    return this.authService.signup();
+    return this.authService.signup(dto);
   }
 
   @Post('signin')
   signIn() {
-   return this.authService.signin();
+    return this.authService.signin();
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,29 @@
+import * as argon from 'argon2';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
   constructor(private prima: PrismaService) {}
 
-  signin() {
-    return 'I have signed in';
+  async signup(dto: AuthDto) {
+    const { password, email } = dto;
+    const hash = await argon.hash(password);
+
+    const user = await this.prima.user.create({
+      data: {
+        email,
+        hash,
+      },
+    });
+
+    delete user.hash;
+
+    return user;
   }
 
-  signup() {
+  signin() {
     return 'I have signed up';
   }
 }
