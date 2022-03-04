06e10d564e55fc0f359835411cff9c4535fa7db7
Alexander <Alexander_alatorre1993@hotmail.com>
2022-03-03 19:45:35 -0700

    added patch logic

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { EditUserDto } from './dto';
 
 @Injectable()
 export class UserService {
-    async editUser(userId: number) {
-        
-    }
+  constructor(private prisma: PrismaService) {}
+
+  async editUser(userId: number, dto: EditUserDto) {
+    const userIdMatches = { id: userId };
+    const updatedUserData = { ...dto };
+
+    const user = await this.prisma.user.update({
+      where: userIdMatches,
+      data: updatedUserData,
+    });
+
+    delete user.hash;
+
+    return user;
+  }
 }
